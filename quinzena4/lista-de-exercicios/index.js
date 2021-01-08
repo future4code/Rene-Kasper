91ce27f0f6ad60f8c56a6d42182366cb7873853c
Rene <renerkasper@gmail.com>
2021-01-08 14:52:37 -0300

    Exercício 01 de Interpretação

--- quinzena4/lista-de-exercicios/index.js
+++ quinzena4/lista-de-exercicios/index.js
@@ -1,1 +1,5 @@
 // Projeto 4: Lista de Exercícios //
+
+// ********************* EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO ********************** //
+// EXERCÍCIO 01 //
+//O valor impresso será uma multiplicação da cotação por 100, resultando na saída de "R$ valor*100"
