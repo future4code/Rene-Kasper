1c8fb1fe751044a992e4f7b3842f253d0a07fec9
Rene <renerkasper@gmail.com>
2021-01-08 16:55:38 -0300

    Exercício 03 de Funções de Arrays

--- quinzena4/lista-de-exercicios/index.js
+++ quinzena4/lista-de-exercicios/index.js
@@ -208,3 +208,32 @@ function parOuImpar(n) {
 
 console.log(valoresTriplicados)
 console.log(parImpar)
+
+//EXERCÍCIO 02
+const visitantes = [
+	{ nome: "Paula", idade: 12, altura: 1.8},
+	{ nome: "João", idade: 20, altura: 1.3},
+	{ nome: "Pedro", idade: 15, altura: 1.9},
+	{ nome: "Luciano", idade: 22, altura: 1.8},
+	{ nome: "Artur", idade: 10, altura: 1.2},
+	{ nome: "Soter", idade: 70, altura: 1.9}
+]
+
+const permitido = visitantes.filter((visitante) => {
+    if((visitante.altura >= 1.5) && (visitante.idade >= 14) && (visitante.idade < 60)) {
+        return true
+    } else {
+        return false
+    }
+})
+
+const naoPermitido = visitantes.filter((visitante) => {
+    if((visitante.altura < 1.5) || (visitante.idade < 14) || (visitante.idade > 60)) {
+        return true
+    } else {
+        return false
+    }
+})
+
+console.log(permitido)
+console.log(naoPermitido)
