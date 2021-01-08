328254caf6936899bb68a0f451c24c0d09e28018
Rene <renerkasper@gmail.com>
2021-01-08 14:53:01 -0300

    Exercício 02 de Interpretação

--- quinzena4/lista-de-exercicios/index.js
+++ quinzena4/lista-de-exercicios/index.js
@@ -3,3 +3,6 @@
 // ********************* EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO ********************** //
 // EXERCÍCIO 01 //
 //O valor impresso será uma multiplicação da cotação por 100, resultando na saída de "R$ valor*100"
+
+// EXERCÍCIO 02 //
+// O valor impresso na variável novoMontante será de 165 e na variável segundoMontante será "Tipo de investimento informado incorreto!"
