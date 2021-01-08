141f2d9ffad43a3a7f6a11fb77a9ad59a932b39d
Rene <renerkasper@gmail.com>
2021-01-08 17:12:47 -0300

    Exercício 05 de Funções de Arrays

--- quinzena4/lista-de-exercicios/index.js
+++ quinzena4/lista-de-exercicios/index.js
@@ -261,3 +261,26 @@ const confirmadas = consultas.map((paciente) => {
         }
     }
 });
+
+//EXERCÍCIO 05
+const contas = [
+	{ cliente: "João", saldoTotal: 1000, compras: [100, 200, 300] },
+	{ cliente: "Paula", saldoTotal: 7500, compras: [200, 1040] },
+	{ cliente: "Pedro", saldoTotal: 10000, compras: [5140, 6100, 100, 2000] },
+	{ cliente: "Luciano", saldoTotal: 100, compras: [100, 200, 1700] },
+	{ cliente: "Artur", saldoTotal: 1800, compras: [200, 300] },
+	{ cliente: "Soter", saldoTotal: 1200, compras: [] }
+]
+
+function saldo() {
+    contas.forEach((cliente) => {
+    let gastos = 0
+        for(let gasto of cliente.compras) {
+            gastos += gasto
+        }
+        cliente.saldoTotal -= gastos
+    })
+    return contas
+}
+
+console.log(saldo())
